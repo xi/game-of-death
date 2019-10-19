868d2c1c56819a002ec2aca272cde1ca9d4fd6fe
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-10-19 15:10:18 +0200

    basic event

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ var preact = require('preact');
 var template = require('./template.js');
 var logic = require('./logic.js');
 
+var wrapper = document.body;
+
 var state = {
     board: logic.setupBoard(),
 };
 
-var tree = template(state);
-preact.render(tree, document.body);
+preact.render(template(state), wrapper);
+
+document.addEventListener('click', function(event) {
+    state.board[0][0] += 1;
+    preact.render(template(state), wrapper);
+});
