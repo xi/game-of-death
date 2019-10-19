9fcf68af846b7e786cdb6b83b62a4ddbd1bbd3e4
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-10-19 15:49:43 +0200

    integrate update() in on()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var vdom = require('petit-dom/dist/petit-dom.min');
 var template = require('./template.js');
 var logic = require('./logic.js');
-var _ = require('./helpers.js');
 
 var state, tree;
 
@@ -20,18 +19,26 @@ var update = function() {
     tree = newTree;
 };
 
-_.on('click', '.board-cell', function(event) {
+var on = function(eventType, selector, fn) {
+    document.addEventListener(eventType, function(event) {
+        var target = event.target.closest(selector);
+        if (target) {
+            fn.call(target, event);
+            update();
+        }
+    });
+};
+
+on('click', '.board-cell', function(event) {
     var row = this.parentElement;
     var board = row.parentElement;
     var x = Array.prototype.indexOf.call(row.children, this);
     var y = Array.prototype.indexOf.call(board.children, row);
     state.board[y][x] = (state.board[y][x] + 1) % 5;
-    update()
 });
 
-_.on('click', '.js-next-gen', function(event) {
+on('click', '.js-next-gen', function(event) {
     logic.calculateNextGen(state);
-    update();
 });
 
 init(document.body);
