f0ebda62f5ab26084a9ad01d439048ebf700721d
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-10-19 15:48:21 +0200

    use petit-dom instead of preact

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-var preact = require('preact');
+var vdom = require('petit-dom/dist/petit-dom.min');
 var template = require('./template.js');
 var logic = require('./logic.js');
 var _ = require('./helpers.js');
 
-var wrapper = document.body;
+var state, tree;
 
-var state = {
-    board: logic.setupBoard(),
+var init = function(wrapper) {
+    state = {
+        board: logic.setupBoard(),
+    };
+    tree = template(state);
+    var element = vdom.mount(tree);
+    wrapper.append(element);
+}
+
+var update = function() {
+    newTree = template(state);
+    vdom.patch(newTree, tree);
+    tree = newTree;
 };
-
-preact.render(template(state), wrapper);
 
 _.on('click', '.board-cell', function(event) {
     var row = this.parentElement;
@@ -17,10 +26,12 @@ _.on('click', '.board-cell', function(event) {
     var x = Array.prototype.indexOf.call(row.children, this);
     var y = Array.prototype.indexOf.call(board.children, row);
     state.board[y][x] = (state.board[y][x] + 1) % 5;
-    preact.render(template(state), wrapper);
+    update()
 });
 
 _.on('click', '.js-next-gen', function(event) {
     logic.calculateNextGen(state);
-    preact.render(template(state), wrapper);
+    update();
 });
+
+init(document.body);
