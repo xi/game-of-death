56d1f0a1a139122a0fb57fdc62d288e8262a2db3
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-10-19 15:23:53 +0200

    use actual logic

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ var state = {
 preact.render(template(state), wrapper);
 
 _.on('click', '.board-cell', function(event) {
-    state.board[0][0] += 1;
+    var row = this.parentElement;
+    var board = row.parentElement;
+    var x = Array.prototype.indexOf.call(row.children, this);
+    var y = Array.prototype.indexOf.call(board.children, row);
+    state.board[y][x] = (state.board[y][x] + 1) % 5;
+    preact.render(template(state), wrapper);
+});
+
+_.on('click', '.js-next-gen', function(event) {
+    console.log('foo');
+    logic.calculateNextGen(state);
     preact.render(template(state), wrapper);
 });
