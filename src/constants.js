c6db1b878afe4bc9741a14552f27dc8f3f123e63
radow <masteroftheriddles@googlemail.com>
2019-10-19 14:54:54 +0200

    CalculateNextGen

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,7 @@
 module.exports = {
     EMPTY: 0,
-    PLAYERONE: 1,
-    PLAYERTWO: 2,
 
-    hight: 60,
+    playerCount: 5,
+    height: 60,
     width: 40,
 }
--- src/logic.js
+++ src/logic.js
@@ -5,20 +5,68 @@ var example = function(a, b) {
 
 var setupBoard = function(){
     var board = [];
-    for(var i = 0; i< constants.width; i++){
-        board[i] = [];
-        for(var j = 0; j<constants.hight; j++){
-            board[i][j] = constants.EMPTY;
+    for(var x = 0; x< constants.width; x++){
+        board[x] = [];
+        for(var y = 0; y<constants.height; y++){
+            board[x][y] = constants.EMPTY;
         }
     }
     return board;
 };
 
 var calculateNextGen = function (board){
+    calcBoard = [];
 
+    //Calculate every player seperatly
+    for(var p = 0; p< constants.playerCount; p++){
+        calcBoard[p] = [];
+        for(var x = 0; x< constants.width; x++){
+            calcBoard[p][x] = [];
+            for(var y = 0; y<constants.height; y++){
+                var friendlyNeighboars = getFriendlyNeighboars(board, x, y, p);
+                // Rules are here!
+                if((board[x][y] == p && friendlyNeighboars > 1 && friendlyNeighboars < 4)||
+                   (board[x][y] != p && friendlyNeighboars == 3)
+                    ) calcBoard[p][x][y] = p;
+                else calcBoard[p][x][y] = constants.EMPTY;
+            }
+        }
+    }
+
+    //Conflate all playerevolutions by clearing tiles that would be claimed by multiple players
+    for(var x = 0; x< constants.width; x++){
+        for(var y = 0; y<constants.height; y++){
+            var empty = true;
+            for(var p = 0; p< constants.playerCount; p++){
+                if(calcBoard[p][x][y] == p){
+                    if(empty == false){
+                        board[x][y] = constants.EMPTY;
+                        continue;
+                    }
+                    else {
+                        empty = false;
+                        board[x][y] = p;
+                    }
+                }
+            }
+        }
+    }
+
+    return board;
+}
+
+var getFriendlyNeighboars = function(board, x, y, p){
+    var count = 0;
+    for(var deltaX = -1; deltaX <= 1; deltaX++){
+        for(var deltaY = -1; deltaY <= 1; deltaY++){
+            if((deltaX != 0 || deltaY != 0) && board[x + deltaX][y + deltaY] == p) count ++;
+        }
+    }
+    return count;
 }
 
 module.exports = {
     example: example,
     setupBoard: setupBoard,
+    calculateNextGen: calculateNextGen,
 }
