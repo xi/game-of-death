4048050085296969c7941b50bda6515b7d3cff4b
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-10-26 16:19:12 +0200

    add spaces between board controls

--- src/template.js
+++ src/template.js
@@ -21,19 +21,29 @@ const renderControls = function(state) {
     if (state.game.sandbox) {
         return h('div', {'class': 'board-controls'}, [
             h('input', {type: 'range', value: 50, name: 'speed'}),
+            ' ',
             h('input', {type: 'number', value: 1, name: 'steps'}),
+            ' ',
             h('button', {'class': 'js-next-gen'}, 'Next Gen'),
+            ' ',
             h('button', {'class': 'js-play'}, state.game.playing ? 'Pause' : 'Play'),
+            ' ',
             h('button', {'class': 'js-current-player fg-' + state.game.currentPlayer}, 'Current Player'),
+            ' ',
             h('button', {'class': 'js-export'}, 'Export'),
+            ' ',
             h('button', {'class': 'js-quit'}, 'Quit'),
         ]);
     } else {
         return h('div', {'class': 'board-controls'}, [
             h('input', {type: 'range', value: 50, name: 'speed'}),
+            ' ',
             h('input', {type: 'hidden', value: 50, name: 'speed'}),
+            ' ',
             h('input', {type: 'hidden', value: 1, name: 'steps'}),
+            ' ',
             h('button', {'class': 'js-play'}, state.game.playing ? 'Pause' : 'Play'),
+            ' ',
             h('button', {'class': 'js-quit'}, 'Quit'),
         ]);
     }
