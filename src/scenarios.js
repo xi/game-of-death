f09db0ef317d314db4c63afa7919c15cd9adfdea
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-11-01 17:00:44 +0100

    generate scenarios.js

--- src/scenarios.js
+++ src/scenarios.js
@@ -2,18 +2,20 @@ import s01_survive_01 from '../scenarios/01_survive_01.js';
 import s02_growth_01 from '../scenarios/02_growth_01.js';
 import s03_reach_01 from '../scenarios/03_reach_01.js';
 import s04_growth_02 from '../scenarios/04_growth_02.js';
-import s05_glider_01 from '../scenarios/05_glider_01.js';
-import s06_0reach_02 from '../scenarios/06_0reach_02.js';
-import s07_glider_02 from '../scenarios/07_glider_02.js';
-import s08_gun from '../scenarios/08_gun.js';
+import s05_symbiosis from '../scenarios/05_symbiosis.js';
+import s06_glider_01 from '../scenarios/06_glider_01.js';
+import s07_reach_02 from '../scenarios/07_reach_02.js';
+import s08_glider_02 from '../scenarios/08_glider_02.js';
+import s09_gun from '../scenarios/09_gun.js';
 
 export default [
     s01_survive_01,
     s02_growth_01,
     s03_reach_01,
     s04_growth_02,
-    s05_glider_01,
-    s06_0reach_02,
-    s07_glider_02,
-    s08_gun,
+    s05_symbiosis,
+    s06_glider_01,
+    s07_reach_02,
+    s08_glider_02,
+    s09_gun,
 ];
