62b92401a36b02dba1f0078cdc47a3e2ee5f141f
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-11-01 15:33:03 +0100

    add pre-gun scenarios

--- src/scenarios.js
+++ src/scenarios.js
@@ -4,12 +4,16 @@ import growth1 from '../scenarios/02_01growth.js';
 import growth2 from '../scenarios/02_02growth.js';
 import reach1 from '../scenarios/03_01reach.js';
 import reach2 from '../scenarios/03_02reach.js';
+import glider1 from '../scenarios/04_01glider.js';
+import glider2 from '../scenarios/04_02glider.js';
 
 export default [
     survive1,
     growth1,
     reach1,
     growth2,
+    glider1,
     reach2,
+    glider2,
     gun,
 ];
