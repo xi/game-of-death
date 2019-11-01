3a20813daf38748b4ba318f3942530ab38669ede
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-11-01 15:00:44 +0100

    structure scenarios

--- src/scenarios.js
+++ src/scenarios.js
@@ -1,19 +1,15 @@
 import gun from '../scenarios/gun.js';
 import survive1 from '../scenarios/01_01survive.js';
-import survive2 from '../scenarios/01_02survive.js';
-import survive3 from '../scenarios/01_03survive.js';
 import growth1 from '../scenarios/02_01growth.js';
 import growth2 from '../scenarios/02_02growth.js';
 import reach1 from '../scenarios/03_01reach.js';
 import reach2 from '../scenarios/03_02reach.js';
 
 export default [
-    gun,
     survive1,
-    survive2,
-    survive3,
     growth1,
-    growth2,
     reach1,
+    growth2,
     reach2,
+    gun,
 ];
